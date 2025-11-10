19f17df8e99622e576ec4c17c658ce8c3ecdaa7f
Reynard <reynardgeovanni@gmail.com>
2025-11-10 21:35:04 +0700

    Commit No. 3 – UI Element and Theme

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -16,6 +16,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,7 +80,7 @@ fun HomeContent(
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = stringResource(id = R.string.enter_item))
+                OnBackgroundTitleText(text = stringResource(id = R.string.enter_item))
 
                 TextField(
                     value = inputField.name,
@@ -114,4 +117,37 @@ fun HomeContent(
 @Composable
 fun PreviewHome() {
     Home(listOf("Tanu", "Tina", "Tono"))
+}
+
+// ========== UI ELEMENTS ==========
+@Composable
+fun OnBackgroundTitleText(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleLarge,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+}
+
+@Composable
+fun OnBackgroundItemText(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodySmall,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+}
+
+@Composable
+fun PrimaryTextButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.padding(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.DarkGray,
+            contentColor = Color.White
+        )
+    ) {
+        Text(text = text, style = MaterialTheme.typography.labelMedium)
+    }
 }
